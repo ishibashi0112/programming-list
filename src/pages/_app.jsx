b1a2c0b581125f88c53b3ce03191e5a15d3d20c2
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-22 21:57:45 +0900

    Headコンポーネント位置変更

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import React from "react";
 import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
