cb618bff8a7358967f822f1fa34f6d43607ff50c
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-22 22:01:42 +0900

    newFormコンポーネント　作成

---
+++ src/components/NewForm/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Folder } from "src/components/Folder";
+import { Header } from "src/components/Header";
+
+export const NewForm = () => {
+  return <div></div>;
+};
