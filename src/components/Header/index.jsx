e9e2bfa19ece333595c85d5fa3995c0270a0cc60
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-22 22:01:15 +0900

    Headerコンポーネント　作成

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="flex items-center h-12 px-4 border-b border-black ">
+      <h1 className="text-2xl font-bold">Prgramming List</h1>
+    </header>
+  );
+};
