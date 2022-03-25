7f211106e7063c0e016818251e6cd012b646f829
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-25 09:40:21 +0900

    ✨ rootへリダイレクト

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <header className="flex items-center h-12 px-4 border-b border-black ">
-      <h1 className="text-2xl font-bold">Prgramming List</h1>
+      <Link href="/">
+        <a>
+          <h1 className="text-2xl font-bold">Prgramming List</h1>
+        </a>
+      </Link>
     </header>
   );
 };
