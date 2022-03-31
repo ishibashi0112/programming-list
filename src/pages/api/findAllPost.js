42c2468581329707c771c6f8759c5f43f591ef16
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-31 14:10:49 +0900

    不要ファイル削除

--- src/pages/api/findAllPost.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "src/utils/prismaClient";
-
-export default async (req, res) => {
-  const posts = await prisma.post.findMany();
-
-  if (posts) {
-    res.status(200).json(posts);
-  }
-};
